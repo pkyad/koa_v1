50198e522186deb1038e2559cb7002c4e719cb67
Pradeep Yadav <pradeepyadav@Pradeeps-MacBook-Air.local>
2023-06-09 17:54:43 +0530

    feat: commitlint and pre push hook test

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,24 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverageFrom: [
-    '<rootDir>/app/**/*.{ts,js}',
-    '!<rootDir>/app/models/**/*.{ts,tsx,js,jsx}',
-  ],
-  modulePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
-  testMatch: ['<rootDir>/__test__/**/?(*.)+(spec|test).[jt]s'],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    '^@/(.*)$': '<rootDir>/app/$1',
-  },
-  coverageThreshold: {
-    global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
-    },
-  },
-};
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	collectCoverageFrom: [
+		'<rootDir>/app/**/*.{ts,js}',
+		'!<rootDir>/app/models/**/*.{ts,js}',
+		'!<rootDir>/app/services/**/*.{ts,js}'
+	],
+	modulePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+	testMatch: ['<rootDir>/__test__/**/?(*.)+(spec|test).[jt]s'],
+	moduleNameMapper: {
+		// Handle module aliases (this will be automatically configured for you soon)
+		'^@/(.*)$': '<rootDir>/app/$1'
+	},
+	coverageThreshold: {
+		global: {
+			branches: 50,
+			functions: 50,
+			lines: 50,
+			statements: 50
+		}
+	}
+}
